3a5cc033fa7aba1b35e26bc5d71e0bc19115acc9
James Kong <james.kong@nixplay.com>
2017-05-29 17:37:07 +0800

    Added Android srouce

---
+++ src/android/com/creedon/nixplay/photobrowser/PhotoBrowserPlugin.java
@@ -0,0 +1,44 @@
+PhotoBrowserPlugin
+
+package com.creedon.nixplay.photobrowser.PhotoBrowserPlugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class PhotoBrowserPlugin extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("showGallery")) {
+            String message = args.getString(0);
+            this.showGallery(message, callbackContext);
+            return true;
+        }
+        if (action.equals("showBrowser")) {
+            String message = args.getString(0);
+            this.showBrowser(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void showGallery(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+    private void showBrowser(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
